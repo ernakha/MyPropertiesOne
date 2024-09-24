ec6b62fbc5cd36091ce9663cc3fae8aad4cfca51
ajil23 <azrilpraya@gmail.com>
2024-09-24 22:54:51 +0800

    fix: fixing grafik & in dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,23 +3,28 @@
     <div class="container-fluid">
         <!--  Row 1 -->
         <div class="row">
-            <div class="col-lg-8 d-flex align-items-strech">
-                <div class="card w-100">
-                    <div class="card-body">
-                        <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
-                            <div class="mb-3 mb-sm-0">
-                                <h5 class="card-title fw-semibold">Grafik Data Properti</h5>
-                            </div>
-                        </div>
-                        <div style="width: 90%; margin: auto;">
-                            <canvas id="propertyChart"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="col-lg-4">
                 <div class="row">
                     <div class="col-lg-12">
+                      {{-- hello --}}
+                      <div class="card">
+                          <div class="card-body">
+                              <div class="row alig n-items-start">
+                                  <div class="col-8">
+                                      <h5 class="card-title mb-9 fw-semibold">Selamat Datang, </h5>
+                                      <h4 class="fw-semibold mb-3">{{Auth::user()->name}}</h4>
+                                  </div>
+                                  <div class="col-4">
+                                      <div class="d-flex justify-content-end">
+                                          <div
+                                              class="text-white bg-secondary rounded-circle p-6 d-flex align-items-center justify-content-center">
+                                              <i class="ti ti-user fs-6"></i>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
                         <!-- Total Properti -->
                         <div class="card">
                             <div class="card-body">
@@ -38,7 +43,6 @@
                                     </div>
                                 </div>
                             </div>
-                            <div id="earning"></div>
                         </div>
                         <!-- Total Kota -->
                         <div class="card">
@@ -58,11 +62,25 @@
                                     </div>
                                 </div>
                             </div>
-                            <div id="earning"></div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-lg-8 d-flex align-items-strech">
+                <div class="card w-100">
+                    <div class="card-body">
+                        <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
+                            <div class="mb-3 mb-sm-0">
+                                <h5 class="card-title fw-semibold">Grafik Data Properti</h5>
+                            </div>
+                        </div>
+                        <div style="width: 100%; margin: auto;">
+                            <canvas id="propertyChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
